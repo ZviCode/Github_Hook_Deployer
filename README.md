3e427a693f1e14f80d1d84851ff18fc6a8620e2f
zviCode <t7647986@gmail.com>
2024-04-24 16:17:37 +0000

    add file

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Webhook_GitHub_Server_Side
-Server-side GitHub management and hooking, and updating or installing a Docker image
+
+# How to use the project
+
+1. clone the project from github
+
+```
+git clone https://github.com/ZviCode/Github_Hook_Deployer.git
+```
+
+2. change directory to the project directory
+```
+cd Github_Hook_Deployer
+```
+
+3. install the requirements using pip
+```
+pip install -r requirements.txt
+```
+
+
+4. set .env file with the following variables
+* ```TELEGRAM_TOKEN=your_telegram_token```
+* ```ID_TELEGRAM_FOR_LOG=your_telegram_id```
+* ```PORT=your_port```
+
+5. run the project using python
+```
+python app.py
+```
+
+6. create a webhook on github that points to the server where the project is running
+* ```http://your_server_ip:your_port```
+
+7. push code to the repository and see the webhook in action
+
+8. check the logs in the telegram bot
+
+9. enjoy the webhook
+
+
+
+This is the folder tree that needs to be in your project for it to work
+___________
+main :
+- app.py
+- docker-compose.yml
+
+- - project
+- - project2
---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, abort
+import os
+import requests
+import subprocess
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+list_last_actions_head_sha = []
+bot_telegram_token = os.getenv('BOT_TELEGRAM_TOKEN')
+id_telegram_for_log = os.getenv('ID_TELEGRAM_FOR_LOG')
+port = os.getenv('PORT')
+
+docker_compose_path = f'./docker-compose.yml'
+def send_message_telegram(message):
+    formatted_message = message.replace('_', '\\_')
+    url = f'https://api.telegram.org/bot{bot_telegram_token}/sendMessage'
+    params = {'chat_id': id_telegram_for_log, 'text': formatted_message, 'parse_mode': 'MarkdownV2'}
+    r = requests.get(url, params=params)
+    return r.json()
+    
+
+send_message_telegram(f'🚀 *Webhook server started* \n running on port {port}')
+
+
+def get_container_id(name):
+    result = subprocess.run(['docker', 'ps', '-a', '--format', '{{.ID}} {{.Names}}'],
+                            capture_output=True, text=True)
+    for line in result.stdout.strip().split('\n'):
+        container_id, container_name = line.split(' ', 1)
+        if name in container_name:
+            return container_id
+    return None
+
+def valid_head_sha(head_sha, service_name):
+    if not head_sha or head_sha in list_last_actions_head_sha:
+        return False
+    list_last_actions_head_sha.append(head_sha)
+    if len(list_last_actions_head_sha) > 5:
+        list_last_actions_head_sha.pop(0)
+    send_message_telegram(f"🔗 *New commit for {service_name}:* ```{head_sha}```")
+    return True
+
+ 
+def chack_docker_compose_file(service_name, docker_compose_path):
+    if not os.path.exists(docker_compose_path):
+        send_message_telegram(f"⚠️ *No docker\-compose file found for {service_name}*")
+        abort(400, 'Docker-compose file not found')
+    with open(docker_compose_path, 'r') as file:
+        for line in file:
+            if service_name in line:
+                return True
+        send_message_telegram(f"⚠️ *No service found in docker\-compose file for {service_name}*")
+        return False
+
+
+def update_docker_compose(service_name, docker_compose_path):
+    try:
+        subprocess.run(['git', '-C', f'./{service_name}', 'pull'], check=True)
+        send_message_telegram(f"🔄 *Successfully pulled {service_name}*")
+        subprocess.run(['docker-compose', '-f', docker_compose_path, 'build', service_name], check=True)
+        send_message_telegram(f"🔨 *Successfully built {service_name}*")
+        manage_docker_container(service_name)
+        subprocess.run(['docker-compose', '-f', docker_compose_path, 'up', '-d', service_name], check=True)
+        send_message_telegram(f"🚀 *Successfully updated and scaled {service_name}*")
+    except subprocess.CalledProcessError as e:
+        send_message_telegram(f"⚠️ *Failed to update service {service_name}:* ```{e}```")
+        abort(500, f'Failed to update service {service_name}: {e}')
+
+def manage_docker_container(service_name):
+    id_container = get_container_id(service_name)
+    if id_container:
+        subprocess.run(['docker', 'stop', id_container], check=True)
+        send_message_telegram(f"⏹️ *Successfully stopped {service_name}*")
+        subprocess.run(['docker', 'rm', id_container], check=True)
+        send_message_telegram(f"🗑️ *Successfully removed {service_name}*")
+        
+        
+@app.route('/webhook', methods=['POST'])
+def handle_webhook():
+    data = request.get_json()
+    action = data.get('action', 'undefined')
+    head_sha = data.get('check_run', {}).get('head_sha')
+    service_name = data['repository']['name']
+    if chack_docker_compose_file(service_name, docker_compose_path):
+        if action == 'completed':
+            if valid_head_sha(head_sha, service_name):
+                update_docker_compose(service_name, docker_compose_path)
+            return 'Webhook received and processed', 200
+        return 'Webhook received and processed', 200
+    return 'Webhook received and ignored', 200
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=port)
